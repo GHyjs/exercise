7304be39cbe3ca2f1b5214fa69ba24868a86626f
GHyjs <yangyangjianshu@163.com>
2020-04-25 20:50:05 +0800

    exercise

---
+++ gujuwang/js/index.js
@@ -0,0 +1,9 @@
+// JavaScript Documentvar flow = getElementByClass('flow1');
+function mouseMM(obj){
+	obj.style.backgroundColor = "#73C741";
+	obj.style.color = "#FFFFFF";
+}
+
+$(document).ready(function(){
+	$('.pp:lt(3)').css('color','#F25618');
+});
